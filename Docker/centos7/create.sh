9e5b6d739e772241b7d72ef824c570b66b1ac2cb
Nick <nick.telindert@metgezondboerenverstand.nl>
2020-08-08 23:04:05 +0200

    Focus on Fedora images

--- Docker/centos7/create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker build -t nextspace-dev .
-docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-dev nextspace-dev:latest
-docker start nextspace-dev
-docker exec -w / -it nextspace-dev git clone https://github.com/nicktelindert/nextspace-build-iso.git
-docker exec -w /nextspace-build-iso -it nextspace-dev sh build.sh
-docker cp nextspace-dev:/nextspace-build-iso/NEXTSPACE.iso ~/NEXTSPACE.iso
-docker stop nextspace-dev
-docker container rm nextspace-dev
