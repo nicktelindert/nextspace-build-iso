97188145e6ca1472d570cfbc9295c33ca49486db
Nick te Lindert <nick.telindert@metgezondboerenverstand.nl>
2020-08-09 13:03:07 +0200

    Centos 8 working

--- Docker/centos8/create.sh
+++ Docker/centos8/create.sh
@@ -2,7 +2,7 @@
 docker build -t nextspace-dev .
 docker create  -v /dev:/dev --privileged=true -ti  --name nextspace-dev nextspace-dev:latest
 docker start nextspace-dev
-docker exec -w / -it nextspace-dev git clone https://github.com/nicktelindert/nextspace-build-iso.git
+docker exec -w / -it nextspace-dev git clone -b feature/centos-8 https://github.com/nicktelindert/nextspace-build-iso.git
 docker exec -w /nextspace-build-iso -it nextspace-dev sh build.sh
 docker cp nextspace-dev:/nextspace-build-iso/NEXTSPACE.iso ~/NEXTSPACE.iso
 docker stop nextspace-dev
